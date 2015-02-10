3b1949749c3876e77c6bc89cf8fece90f49ece09
Brian Capouch <brianc@saintjoe.edu>
2015-02-10 18:33:00 -0500

    Manually added missing apostrophes

--- larder.js
+++ larder.js
@@ -1,10 +1,11 @@
-var LARDER = "[\n  " + [
-{ "labelID": "24", "name": "WW flour", "quantity": 11, "units": "lb", "dateIn": "2/1/15", "life": 45  },
-{ "labelID": "25", "name": "Avocado", "quantity": 3, "units": "ea", "dateIn": "2/1/15", "life": 10  },
-{ "labelID": "26", "name": "milk", "quantity": 1, "units": "half-gal", "dateIn": "2/6/15", "life": 8  },
-{ "labelID": "27", "name": "butter", "quantity": 2, "units": "lb", "dateIn": "2/1/15", "life": 20  },
-{ "labelID": "28", "name": "salt", "quantity": 1, "units": "lb", "dateIn": "1/20/15", "life": 365  },
-{ "labelID": "29", "name": "Cream cheese", "quantity": 3, "units": "lb", "dateIn": "1/10/15", "life": 45  }
+var LARDER_FILE = "[\n  " + [
+'{ "labelID": "24", "name": "WW flour", "quantity": 11, "units": "lb", "dateIn": "2/1/15", "life": 45  }',
+'{ "labelID": "25", "name": "Avocado", "quantity": 3, "units": "ea", "dateIn": "2/1/15", "life": 10  }',
+'{ "labelID": "26", "name": "milk", "quantity": 1, "units": "half-gal", "dateIn": "2/6/15", "life": 8  }',
+'{ "labelID": "27", "name": "butter", "quantity": 2, "units": "lb", "dateIn": "2/1/15", "life": 20  }',
+'{ "labelID": "28", "name": "salt", "quantity": 1, "units": "lb", "dateIn": "1/20/15", "life": 365  }',
+'{ "labelID": "29", "name": "Cream cheese", "quantity": 3, "units": "lb", "dateIn": "1/10/15", "life": 45 }',
+'{ "labelID": "30", "name": "Bread flour", "quantity": 10, "units": "lb", "dateIn": "2/7/15", "life": 45  }'
 ].join(",\n  ") + "\n]";
 
 // This makes sure the data is exported in node.js

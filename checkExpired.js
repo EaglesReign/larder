c9f1baba86d3b0aac074b688b20ed6746005e420
Brian Capouch <brianc@saintjoe.edu>
2015-02-10 22:55:02 -0500

    Clean up and document

--- checkExpired.js
+++ checkExpired.js
@@ -1,10 +1,19 @@
+/* 
+	checkExpired.js
+	Filter larder dataset looking for expired items
+	Brian Capouch CMP237
+	10 February 2015
+						*/
+
 load('larder.js');
 var larder = JSON.parse(LARDER_FILE);
 
+// Print entire contents 
 //larder.forEach(larder, function(entry) {
 //	print(JSON.stringify(entry));
 //});
 
+// Test 0th item for date conversion
 // var testDate = new Date(larder[0]["dateIn"]);
 // print(testDate.toDateString());
 
@@ -19,10 +28,13 @@ function filter(array, test) {
 }
 
 // Next two lines removed from anonymous function below
+// This line calculates expiration date in ms
 // print(((new Date(item.dateIn)).getTime()) + (item.life * 86400000));
+
+// This line expresses today's date in ms
 // print((new Date()).getTime());
 
-// Right out of the book except for test part
+// Right out of the book EXCEPT for test part
 print(JSON.stringify(filter(larder, function(item) {
   return (((new Date(item.dateIn)).getTime()) + (item.life * 86400000) < (new Date().getTime())) ;
 })));
